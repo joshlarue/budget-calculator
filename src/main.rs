05e24d2245bd886e17a3f2ba457360aa809a67f3
joshlarue <44442031+joshlarue@users.noreply.github.com>
2024-06-18 16:58:04 -0600

    Completed different saving type functions. Gets input from user and returns correct percentages

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,39 @@
-use std::num::ParseIntError;
+use std::num::ParseFloatError;
 fn main() {
-    let monetary_amt = match get_monetary_amt() {
+    // gets input from get_monetary_amt(). Panics if input is invalid
+    // TODO: loop on invalid input
+    let total_monetary_amt = match get_monetary_amt() {
         Ok(num) => num,
-        Err(err) => panic!("Not a number! {}", err),
+        Err(err) => panic!("Invalid input! {}", err),
     };
-    println!("{}", monetary_amt.to_string());
+
+    calculate_to_save_long_term(total_monetary_amt, 0.5);
+    calculate_to_save_medium_term(total_monetary_amt, 0.3);
+    calculate_to_spend_short_term(total_monetary_amt, 0.2);
 }
 
-fn get_monetary_amt() -> Result<i32, ParseIntError> {
+// gets CLI input from user
+fn get_monetary_amt() -> Result<f32, ParseFloatError> {
     let total_monetary_amt = std::env::args()
         .nth(1)
         .expect("No monetary amount given")
-        .parse::<i32>()?;
+        .parse::<f32>()?;
     println!("Monetary amt: ${}", total_monetary_amt.to_string());
     Ok(total_monetary_amt)
 }
 
-//fn calculate_to_save() {}
+// functions to calculate different saving types
+fn calculate_to_save_long_term(total_monetary_amt: f32, percent_to_save: f32) {
+    let total_to_save = total_monetary_amt * percent_to_save;
+    println!("${}: savings account", total_to_save);
+}
+
+fn calculate_to_save_medium_term(total_monetary_amt: f32, percent_to_save: f32) {
+    let total_to_save = total_monetary_amt * percent_to_save;
+    println!("${}: medium-term savings account", total_to_save);
+}
+
+fn calculate_to_spend_short_term(total_monetary_amt: f32, percent_to_save: f32) {
+    let total_to_spend = total_monetary_amt * percent_to_save;
+    println!("${}: short-term spending account", total_to_spend);
+}
